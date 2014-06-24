8f3856112ee9578b137f52aa634f5dbe0d65a4da
jihyun <jihyun.lee@nyu.edu>
2014-06-24 10:30:41 -0400

    NSData+hex added

--- src/ios/CDVBLEFirmata.m
+++ src/ios/CDVBLEFirmata.m
@@ -7,6 +7,7 @@
 //
 
 #import "CDVBLEFirmata.h"
+#import "NSData+hex.h"
 
 @interface CDVBLEFirmata()
 @end
@@ -82,7 +83,6 @@
     [self sendData:newData];
 }
 
-
 - (void)sendData:(NSData*)newData{
     
     //Output data to UART peripheral
@@ -94,29 +94,6 @@
     
 }
 
-- (NSString*)hexRepresentationWithSpaces:(BOOL)spaces{
-    
-    const unsigned char* bytes = (const unsigned char*)[self bytes];
-    NSUInteger nbBytes = [self length];
-    
-    //If spaces is true, insert a space every this many input bytes (twice this many output characters).
-    static const NSUInteger spaceEveryThisManyBytes = 4UL;
-    
-    NSUInteger strLen = 2*nbBytes + (spaces ? nbBytes/spaceEveryThisManyBytes : 0);
-    
-    NSMutableString* hex = [[NSMutableString alloc] initWithCapacity:strLen];
-    for(NSUInteger i=0; i<nbBytes; ) {
-        [hex appendFormat:@"0x%x", bytes[i]];
-        //We need to increment here so that the every-n-bytes computations are right.
-        ++i;
-        
-        if (spaces) {
-            [hex appendString:@" "];
-        }
-    }
-    
-    return hex;
-}
 
 #pragma mark - Cordova Plugin Methods
 
